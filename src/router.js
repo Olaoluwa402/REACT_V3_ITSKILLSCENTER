26831b38f7134bed855ca40346fa08fa4e000ae5
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-02-20 12:46:10 +0100

    form handling and validation

--- src/router.js
+++ src/router.js
@@ -2,26 +2,44 @@ import { Route, Routes, Navigate, Link } from "react-router-dom";
 import Home from "./screens/Home";
 import Register from "./screens/Register";
 import NotFound from "./screens/NotFound";
+import MultiDataForm from "./components/Form/MultiDataForm";
+import Layout from "./components/Layout/Layout/Layout";
 
 const Router = () => {
   return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
+    <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
         <Route path="/not-found" element={<NotFound />} />
+
+        <Route path="/tasks" element={<MultiDataForm />} />
+        {/* <Route path="/tasks/:id" element={<NotFound />} />
+        <Route path="/tasks/:id/edit" element={<NotFound />} /> */}
+
+        {/* <Route path="/posts" element={<NotFound />} />
+        <Route path="/posts/:id" element={<NotFound />} />
+        <Route path="/posts/:id/edit" element={<NotFound />} />
+        <Route path="/posts/:id/comments" element={<NotFound />} />
+        <Route path="/posts/:id/comments/:commetId/edit" element={<NotFound />} /> */}
 
         <Route path="*" element={<Navigate to="/not-found" />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
